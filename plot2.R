b2ba93119e149e53dad02e76b2efaf2fa2f87fa5
idcadmin <idcadmin@idcadmins-MacBook-Pro-2.local>
2015-04-10 15:56:20 +0800

    Commit

---
+++ plot2.R
@@ -0,0 +1,27 @@
+## Download Data from source
+if (!file.exists("./data/household_power_consumption.txt")) {
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl,destfile="./data/power_data.zip",method="curl")
+  # Unzip the file
+  unzip(zipfile="./data/power_data.zip",exdir="./data")
+}
+
+
+## Getting full dataset
+data_full <- read.csv("./data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
